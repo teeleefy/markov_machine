f031288a86e18de4466a9831a767b40ea0b8820d
Tammy <tlpainter3@gmail.com>
2024-02-21 16:18:37 -0600

    adding initial files

---
+++ makeText.js
@@ -0,0 +1,43 @@
+/** Command-line tool to generate Markov text. */
+
+const fs = require('fs');
+const process = require('process');
+const axios = require('axios');
+const async = require('async');
+const { MarkovMachine } = require('./markov');
+
+// let mm = new MarkovMachine(input);
+// console.log(mm.makeText())
+
+const cat = path => {
+    fs.readFile(path, 'utf8', (err, data) => {
+    if (err) {
+        console.log("ERROR: Check local file path.", err);
+        process.exit(1);
+    }
+        let mm = new MarkovMachine(data);
+        console.log(mm.makeText());
+    })
+}
+
+async function webCat(path){
+    try{
+        let response = await axios.get(path);
+        let mm = new MarkovMachine(response.data);
+        console.log(mm.makeText());
+    } catch (err){
+        console.log(err, "ERROR: Check URL path.");
+        process.exit(1);
+    }
+} 
+
+const readFile = (type, path) => {
+  if (type === 'url') {
+  webCat(path);
+} else if (type === 'file'){
+  cat(path);
+}  else{
+    console.error(`Unknown method: ${type}. Please pass in "file" or "url" as first parameter. Then pass in appropriate path. For example: makeText.js file eggs.txt or makeText.js url http://www.gutenberg.org/files/11/11-0.txt.`)
+}
+}
+readFile(process.argv[2], process.argv[3]);
---
+++ markov.js
@@ -0,0 +1,100 @@
+/** Textual markov chain generator */
+
+
+class MarkovMachine {
+
+  /** build markov machine; read in text.*/
+
+  constructor(text) {
+    let words = text.split(/[ \r\n.?!,]+/);
+    this.words = words.filter(c => c !== "");
+    this.chains = this.makeChains();
+  }
+
+  /** set markov chains:
+   *
+   *  for text of "the cat in the hat", chains will be
+   *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
+
+  makeChains() {
+    // TODO
+      let words = this.words;
+      let chainList = {};
+      for (let i = 0; i < words.length; i++){
+        let nextWord = words[i + 1];
+        if (!chainList[words[i]]){
+          chainList[words[i]] = [];
+        }
+        chainList[words[i]].push(nextWord);
+      }
+      return chainList; 
+    }
+    
+  
+
+    
+  
+
+
+  /** return random text from chains */
+
+  makeText(numWords = 100) {
+    // TODO
+    let text = [];
+    let chains = this.chains;
+    let words = Object.keys(this.chains);
+    let firstWord = random(words);
+    let nextMarkovArray;
+    let nextWord = firstWord;
+    let withPeriod;
+    let noPeriod;
+    for (let i = 0; i < numWords; i++){
+      if (text.length === 0){
+        while (!containsUppercase(firstWord)){
+          firstWord = random(words);
+        } 
+        text.push(firstWord);
+        nextWord = firstWord;
+      } else{
+        nextMarkovArray = chains[nextWord];
+        nextWord = random(nextMarkovArray);
+        if (containsUppercase(nextWord)){
+          withPeriod = ". " + nextWord;
+          text.push(withPeriod);
+        }
+        else{
+          noPeriod = " " + nextWord;
+          text.push(noPeriod);
+        }
+      }
+    }
+    let sentences = text;
+    sentences = text.join('');
+    let lastPeriod = (sentences.lastIndexOf('.')+1);
+    sentences = sentences.slice(0, lastPeriod);
+    // console.log(sentences);
+    return sentences;
+  }
+}
+
+function containsUppercase(str) {
+  return /[A-Z]/.test(str);
+}
+
+
+function random(array){
+  let length = (array.length - 1);
+  let index = Math.floor(Math.random()*length);
+  return array[index];
+}
+
+// let input = "I could not, would not, on a boat. I will not, will not, with a goat. I will not eat them in the rain. I will not eat them on a train. Not in the dark! Not in a tree! Not in a car! You let me be! I do not like them in a box. I do not like them with a fox. I will not eat them in a house. I do not like them with a mouse. I do not like them here or there. I do not like them anywhere!"
+
+
+// let mm = new MarkovMachine(input);
+// console.log(mm.makeText())
+
+module.exports = {
+  MarkovMachine: MarkovMachine,
+};
+
